8dd6a628eed012bbc9859b71fc099f406d19a958
Adam <adamdkohler@gmail.com>
2021-04-07 14:48:35 -0700

    playing with tab view

--- SwiftUI-02-Controls/ContentView.swift
+++ SwiftUI-02-Controls/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Tab1View: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Home View")
+    }
+}
+struct Tab2View: View {
+    var body: some View {
+        Text("Highlights")
+    }
+}
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            Tab1View()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Home")
+                }
+
+            Tab2View()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Highlights")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        TabBarView()
     }
 }
--- SwiftUI-02-Controls/SwiftUI_02_ControlsApp.swift
+++ SwiftUI-02-Controls/SwiftUI_02_ControlsApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUI_02_ControlsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarView()
         }
     }
 }
+
+
+
