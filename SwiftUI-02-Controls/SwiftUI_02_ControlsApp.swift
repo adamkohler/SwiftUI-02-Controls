a833f9f4163acc0fb680ed7424c6874b37dc2241
Mike Panitz <michpa@hotmail.com>
2021-04-06 23:03:13 -0700

    Initial Commit

---
+++ SwiftUI-02-Controls/SwiftUI_02_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_02_ControlsApp.swift
+//  SwiftUI-02-Controls
+//
+//  Created by Mike Panitz on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_02_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
